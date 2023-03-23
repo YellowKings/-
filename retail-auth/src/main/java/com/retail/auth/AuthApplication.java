2b203839af23246ac981a8cad427af7a6a98610d
guochangjiang <1287137373@qq.com>
2023-03-23 17:24:16 +0800

    郭长江-authapplication

---
+++ retail-auth/src/main/java/com/retail/auth/AuthApplication.java
@@ -0,0 +1,22 @@
+package com.retail.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 类的说明
+ *
+ * @Author: 手速很慢的King
+ * @DateTime: 2023/02/01 19:48
+ * @Description
+ **/
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableFeignClients
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class);
+    }
+}
